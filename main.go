1785c7c3b31978dfefa8b96775f176f6a8a9b116
Hejiadong <820873255@qq.com>
2019-12-22 09:19:26 +0800

    1.0
    get方法

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ import (
 
 	"github.com/astaxie/beego"
 
-	_ "github.com/go-sql-driver/mysql"
+	//"github.com/astaxie/beego/plugins/cors"
 
+	"github.com/astaxie/beego/context"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // 基本的文件目录结构：routers声明匹配的访问路径（url）
@@ -21,6 +24,17 @@ func init() {
 	// @/ 后面跟的是数据库名
 	orm.RegisterDataBase("default", "mysql", "root:root@tcp(112.125.88.184:3306)/sql_test?charset=utf8")
 	//orm.RunSyncdb("default",true,false)
+	var FilterGateWay = func(ctx *context.Context) {
+		ctx.ResponseWriter.Header().Set("Access-Control-Allow-Origin", "*")
+		//允许访问源
+		ctx.ResponseWriter.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS")
+		//允许post访问
+		ctx.ResponseWriter.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,ContentType,Authorization,accept,accept-encoding, authorization, content-type") //header的类型
+		ctx.ResponseWriter.Header().Set("Access-Control-Max-Age", "1728000")
+		ctx.ResponseWriter.Header().Set("Access-Control-Allow-Credentials", "true")
+	}
+	beego.InsertFilter("*", beego.BeforeRouter, FilterGateWay)
+	//路由设置
 }
 func main() {
 	beego.Run()
--- routers/router.go
+++ routers/router.go
@@ -1,26 +1,28 @@
 package routers
 
 import (
-	"SQLwork/controllers"
 	"SQLwork/database"
 
 	"github.com/astaxie/beego"
+	// "github.com/astaxie/beego/plugins/cors"
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{})
+	//跨域
+
+	// beego.Router("/", &controllers.MainController{})
 	beego.Router("/database", &database.DatabaseController{})
 
 	//测试
-	beego.Router("/test", &database.TestController{})
+	//beego.Router("/test", &database.TestController{})
 	//用户相关
-	beego.Router("/register", &database.RegisterController{}, "get:Get;post:Register")
-	beego.Router("/login", &database.LoginController{}, "get:Get;post:Login")
+	beego.Router("/register", &database.RegisterController{}, "post:Get;get:Register")
+	beego.Router("/login", &database.LoginController{}, "post:Get;get:Login")
 	beego.Router("/logout", &database.LogoutController{}, "get:Logout")
 	beego.Router("/active/?:id", &database.RegisterController{}, "get:Active")
 
 	// 商品相关
-	beego.Router("/goodget", &database.GetGoodsController{}, "post:GetAllGoods")
-	beego.Router("/goodlabelget", &database.GetLabelGoodsController{}, "post:GetLabelGoods")
-	beego.Router("/postgoods", &database.PostGoodsController{}, "post:PostGoods")
+	beego.Router("/goodget", &database.GetGoodsController{}, "get:GetAllGoods")
+	beego.Router("/goodlabelget", &database.GetLabelGoodsController{}, "get:GetLabelGoods")
+	beego.Router("/postgoods", &database.PostGoodsController{}, "get:PostGoods")
 }
